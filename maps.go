b03aba4c0072886c1dc6d03ce11cca2694d10547
levi-w-lucy <reachfan169@gmail.com>
2024-05-17 12:01:54 -0600

    Playing around with pointers as Map keys

--- maps.go
+++ maps.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var google, aws string
+	websites := map[*string]string{
+		&google: "https://google.com",
+		&aws:    "https://aws.com",
+	}
+	fmt.Println(websites)
+	fmt.Println(websites[&google])
+
+	var linkedIn string
+	websites[&linkedIn] = "https://linkedin.com"
+	fmt.Println(websites)
+
+	delete(websites, &google)
+	fmt.Println(websites)
 }
