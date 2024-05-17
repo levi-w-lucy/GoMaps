2950c5a7786346f15515480453dbff4e21514850
levi-w-lucy <reachfan169@gmail.com>
2024-05-17 12:19:10 -0600

    Playing with the Make function

--- maps.go
+++ maps.go
@@ -3,6 +3,19 @@ package main
 import "fmt"
 
 func main() {
+	userNames := make([]string, 0, 5)
+
+	fmt.Println(len(userNames), cap(userNames))
+	userNames = append(userNames, "max")
+	userNames = append(userNames, "manuel")
+	fmt.Println(userNames)
+
+	fmt.Println(len(userNames), cap(userNames))
+	fmt.Println(userNames)
+
+}
+
+func mapTesting() {
 	websites := map[string]string{
 		"Google":              "https://google.com",
 		"Amazon Web Services": "https://aws.com",
